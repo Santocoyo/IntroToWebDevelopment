d70c0f64ffeb1d3a115c3934f75fd64388071427
Alejandro <alesantoscoy@hotmail.com>
2020-12-03 09:51:07 -0600

    Uncomment script

--- clase7/miScript.js
+++ clase7/miScript.js
@@ -1,42 +1,42 @@
-// var a = 10;
+var a = 10;
 
-// console.log(a);
+console.log(a);
 
-// var b = 15;
+var b = 15;
 
-// console.log("Evaluando qué numero es más grande");
+console.log("Evaluando qué numero es más grande");
 
-// if(a > b){
-//     console.log("El numero a es mayor al b");
-// } else{
-//     console.log("El numero b es mayor al a");
-// }
+if(a > b){
+    console.log("El numero a es mayor al b");
+} else{
+    console.log("El numero b es mayor al a");
+}
 
-// console.log("Evaluación completa");
+console.log("Evaluación completa");
 
-// console.log("Decidiendo que tan grande eres");
+console.log("Decidiendo que tan grande eres");
 
-// var edad = 80;
+var edad = 80;
 
-// if(edad < 7){
-//     console.log("Eres una bebe");
-// } else if(edad < 13){
-//     console.log("Eres un niñe");
-// } else if(edad < 18){
-//     console.log("eres un adolescente");
-// } else if(edad < 65){
-//     console.log("Eres un adult@");
-// } else{
-//     console.log("Eres un ancian@")
-// }
+if(edad < 7){
+    console.log("Eres una bebe");
+} else if(edad < 13){
+    console.log("Eres un niñe");
+} else if(edad < 18){
+    console.log("eres un adolescente");
+} else if(edad < 65){
+    console.log("Eres un adult@");
+} else{
+    console.log("Eres un ancian@")
+}
 
-// var condicion = true;
+var condicion = true;
 
-// if(condicion){
-//     console.log("Condición verdadera");
-// } else{
-//     console.log("Condición falsa");
-// }
+if(condicion){
+    console.log("Condición verdadera");
+} else{
+    console.log("Condición falsa");
+}
 
 var password = "hola";
 
